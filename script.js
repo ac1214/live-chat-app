c46ea05e9b46af6b69b690def347cfb07ead1307
A <ac1214@users.noreply.github.com>
2020-03-06 23:52:30 -0700

    update filestructure

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 $(function() {
   console.log("TEST");
+
+  var socket = io();
+
+  $("form").submit(function() {
+    socket.emit("chat message", $("#m").val());
+    $("#m").val("");
+    return false;
+  });
+
+  socket.on("chat message", function(msg) {
+    $("#messages").append($("<li>").text(msg));
+    window.scrollTo(0, document.body.scrollHeight);
+  });
 });
