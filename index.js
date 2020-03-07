73c2f6d0913c0fb1ee541b1e5e29d165df4954f5
A <ac1214@users.noreply.github.com>
2020-03-07 14:25:26 -0700

    update

--- index.js
+++ index.js
@@ -3,21 +3,39 @@ var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var port = process.env.PORT || 3000;
 
+messages = [];
+
+// Route for index.html file
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
+// Route for client-side JavaScript file
 app.get("/script.js", function(req, res) {
   res.sendFile(__dirname + "/script.js");
 });
 
+// Route for CSS file
 app.get("/styles.css", function(req, res) {
   res.sendFile(__dirname + "/styles.css");
 });
 
 io.on("connection", function(socket) {
+  console.log("a user connected");
+
   socket.on("chat message", function(msg) {
-    io.emit("chat message", msg);
+    // TODO: Store user info here too
+    let message = { user: "temp", time: Date.now(), message: msg };
+    messages.push(message);
+
+    io.emit("chat message", message);
+
+    console.log(messages);
+  });
+
+  // Remove user from userlist when disconnected
+  socket.on("disconnecting", reason => {
+    console.log(reason);
   });
 });
 
--- script.js
+++ script.js
@@ -4,13 +4,26 @@ $(function() {
   var socket = io();
 
   $("form").submit(function() {
+    // Prevent empty messages from being sent
+    if ($("#m").val() === "") {
+      return false;
+    }
+
     socket.emit("chat message", $("#m").val());
     $("#m").val("");
     return false;
   });
 
   socket.on("chat message", function(msg) {
-    $("#messages").append($("<li>").text(msg));
+    $("#messages").append($("<li>").text(displayMessages(msg)));
     window.scrollTo(0, document.body.scrollHeight);
   });
 });
+
+function displayMessages(message) {
+  let time = new Date(message.time);
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+
+  return hours + ":" + minutes + " " + message.user + ": " + message.message;
+}
