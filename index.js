4e1fcefbbd1cd77712603bf84eaaaa8142935592
A <ac1214@users.noreply.github.com>
2020-03-06 23:20:38 -0700

    serve from index.js

--- index.js
+++ index.js
@@ -4,15 +4,23 @@ var io = require("socket.io")(http);
 var port = process.env.PORT || 3000;
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/view.html");
+});
+
+app.get("/script.js", function(req, res) {
+  res.sendFile(__dirname + "/script.js");
+});
+
+app.get("/styles.css", function(req, res) {
+  res.sendFile(__dirname + "/styles.css");
 });
 
 io.on("connection", function(socket) {
-    socket.on("chat message", function(msg) {
-        io.emit("chat message", msg);
-    });
+  socket.on("chat message", function(msg) {
+    io.emit("chat message", msg);
+  });
 });
 
 http.listen(port, function() {
-    console.log("listening on *:" + port);
+  console.log("listening on *:" + port);
 });
