0e246ca79d0adf57aaab33e3c754d0b792773cc0
A <ac1214@users.noreply.github.com>
2020-03-08 14:44:36 -0600

    flex working

--- index.js
+++ index.js
@@ -3,42 +3,184 @@ var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var port = process.env.PORT || 3000;
 
-messages = [];
+let messages = [];
+let userList = [];
+
+let usernameNumber = 1;
+let userNumber = 0;
 
 // Route for index.html file
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
 // Route for client-side JavaScript file
 app.get("/script.js", function(req, res) {
-  res.sendFile(__dirname + "/script.js");
+    res.sendFile(__dirname + "/script.js");
 });
 
 // Route for CSS file
 app.get("/styles.css", function(req, res) {
-  res.sendFile(__dirname + "/styles.css");
+    res.sendFile(__dirname + "/styles.css");
 });
 
 io.on("connection", function(socket) {
-  console.log("a user connected");
+    let username = getUniqueUsername();
+    let userColor = "0000ff";
+    let currentUserNumber;
+    userList.push(username);
 
-  socket.on("chat message", function(msg) {
-    // TODO: Store user info here too
-    let message = { user: "temp", time: Date.now(), message: msg };
-    messages.push(message);
+    // Restore previous state from cookies
+    socket.on("restore state", function(cookie) {
+        let cookieArray = cookie.split(";");
+        for (c of cookieArray) {
+            let varVal = c.split("=");
+            if (varVal[0] === "username") {
+                if (checkUniqueUsername(varVal[1])) {
+                    username = varVal[1];
+                } else {
+                    socket.emit("assigned username", username);
+                }
+            } else if (varVal[0] === "color") {
+                if (checkValidColor(varVal[1])) {
+                    userColor = varVal[1];
+                }
+            } else if (varVal[0] === "usernumber") {
+                currentUserNumber = parseInt(varVal[1]);
+            }
+        }
+    });
 
-    io.emit("chat message", message);
+    if (currentUserNumber === undefined) {
+        currentUserNumber = userNumber;
+        userNumber++;
+        socket.emit("assigned number", currentUserNumber);
+    }
 
-    console.log(messages);
-  });
+    // Send the newly connected user chat history
+    socket.emit("chat history", messages);
+    // Send the user the list of people who are online
+    io.emit("online users", userList);
 
-  // Remove user from userlist when disconnected
-  socket.on("disconnecting", reason => {
-    console.log(reason);
-  });
+    socket.on("chat message", function(msg) {
+        // Check if the message is a command
+        if (msg.charAt(0) === "/") {
+            // Get the command
+            let command = msg.substring(1, msg.indexOf(" "));
+            // Get the user input
+            let input = msg.substring(msg.indexOf(" ") + 1);
+
+            // Check what the command is
+            if (command === "nick") {
+                if (!updateNick(input)) {
+                    socket.emit(
+                        "chat message",
+                        "Failed to update nickname, choose a unique username"
+                    );
+                }
+            } else if (command === "nickcolor") {
+                if (!checkValidColor(input)) {
+                    socket.emit(
+                        "chat message",
+                        "Failed to update nickname color, choose a valid color"
+                    );
+                } else {
+                    userColor = input;
+                }
+            } else {
+                socket.emit("chat message", "Invalid command");
+            }
+        } else {
+            // TODO: Store user info here too
+            let message = {
+                user: username,
+                userNumber: currentUserNumber,
+                time: Date.now(),
+                message: msg,
+                color: userColor
+            };
+            messages.push(message);
+
+            io.emit("chat message", message);
+        }
+
+        console.log(messages);
+    });
+
+    function updateNick(newNick) {
+        if (checkUniqueUsername()) {
+            // Replace old username with new one
+            userList[indexOf(username)] = newNick;
+        } else {
+            return false;
+        }
+        // Send the user a new list of people who are online
+        io.emit("online users", userList);
+        return true;
+    }
+
+    function updateColor(newColor) {
+        if (checkUniqueUsername()) {
+            // Update username and remove old one
+            userList.splice(userList.indexOf(username), 1);
+            userList.push(newNick);
+        } else {
+            return false;
+        }
+        // Send the user a new list of people who are online
+        io.emit("online users", userList);
+        return true;
+    }
+
+    // Remove user from userlist when disconnected
+    socket.on("disconnect", reason => {
+        userList.splice(userList.indexOf(username), 1);
+        console.log(reason);
+
+        // Send the users a new list of people who are online
+        io.emit("online users", userList);
+    });
+
+    // Update nick color
+    socket.on("update nickcolor", function(newNickColor) {
+        // TODO: Update nickcolor here
+        console.log(newNickColor);
+    });
 });
 
 http.listen(port, function() {
-  console.log("listening on *:" + port);
+    console.log("listening on *:" + port);
 });
+
+function getUniqueUsername() {
+    let uniqueUsername = "User" + usernameNumber;
+    while (!checkUniqueUsername(uniqueUsername)) {
+        usernameNumber++;
+        uniqueUsername = "User" + usernameNumber;
+    }
+
+    return uniqueUsername;
+}
+
+function checkUniqueUsername(username) {
+    let index = userList.indexOf(username);
+
+    // If the index is -1 it has not been found and it is a unique username
+    if (index == -1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkValidColor(color) {
+    // From: https://www.sitepoint.com/community/t/how-to-check-if-string-is-hexadecimal/162739
+    // Check if a number is hexadecimal value using regex
+    const regex = /[0-9A-Fa-f]{6}/g;
+
+    if (regex.test(color)) {
+        return true;
+    } else {
+        return false;
+    }
+}
--- script.js
+++ script.js
@@ -1,29 +1,91 @@
 $(function() {
-  console.log("TEST");
+    console.log("TEST");
 
-  var socket = io();
+    var socket = io();
+    let username = "";
+    let usernumber;
 
-  $("form").submit(function() {
-    // Prevent empty messages from being sent
-    if ($("#m").val() === "") {
-      return false;
+    $("#userheader").html("You are " + username);
+
+    $("form").submit(function() {
+        // Prevent empty messages from being sent
+        if ($("#m").val() === "") {
+            return false;
+        }
+
+        socket.emit("chat message", $("#m").val());
+        $("#m").val("");
+        return false;
+    });
+
+    // Recieve a message a message
+    socket.on("chat message", function(msg) {
+        addMessage(msg);
+    });
+
+    // Restore chat history
+    socket.on("chat history", function(chatHistory) {
+        for (msg of chatHistory) {
+            addMessage(msg);
+        }
+    });
+
+    function addMessage(msg) {
+        $("#messages").append(
+            $(
+                '<li style="color:#' + msg.color + checkSelfMsg(msg) + ';">'
+            ).text(displayMessages(msg) + usernumber)
+        );
+        $("#messages").scrollTop($("#messages")[0].scrollHeight);
     }
 
-    socket.emit("chat message", $("#m").val());
-    $("#m").val("");
-    return false;
-  });
+    // Check if the message was sent by us and change the number
+    function checkSelfMsg(msg) {
+        return msg.userNumber === usernumber ? ";font-weight:bold" : "";
+    }
 
-  socket.on("chat message", function(msg) {
-    $("#messages").append($("<li>").text(displayMessages(msg)));
-    window.scrollTo(0, document.body.scrollHeight);
-  });
+    // Get list of people online
+    socket.on("online users", function(onlineUsers) {
+        $("#users").empty();
+
+        for (user of onlineUsers) {
+            $("#users").append($("<li>").text(user));
+        }
+        $("#users").scrollTop($("#users")[0].scrollHeight);
+    });
+
+    // When initially connecting to server
+    socket.on("connect", () => {
+        // From: https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie
+        socket.emit("restore state", document.cookie);
+    });
+
+    // Get notified if the username has been assigned
+    socket.on("assigned username", function(assignedUsername) {
+        $("#userheader").html("You are " + assignedUsername);
+        username = assignedUsername;
+        document.cookie = "username=" + username;
+    });
+
+    // Get assigned a username
+    socket.on("assigned color", function(newColor) {
+        document.cookie = "color=" + newColor;
+    });
+
+    // Get assigned a usernumber
+    socket.on("assigned number", function(newNumber) {
+        document.cookie = "usernumber=" + newNumber;
+        usernumber = newNumber;
+    });
+
+    function displayMessages(message) {
+        let time = new Date(message.time);
+        let hours = time.getHours();
+        let minutes = time.getMinutes();
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+
+        return (
+            hours + ":" + minutes + " " + message.user + ": " + message.message
+        );
+    }
 });
-
-function displayMessages(message) {
-  let time = new Date(message.time);
-  let hours = time.getHours();
-  let minutes = time.getMinutes();
-
-  return hours + ":" + minutes + " " + message.user + ": " + message.message;
-}
